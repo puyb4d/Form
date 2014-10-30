7464e972c50b9b23a19a035e605834884bacd4f3
Stéphane Puybareau <stephane.puybareau@4d.com>
2014-10-30 17:06:16 +0100

    Rename to Form. Fields are customizable.

--- designer.js
+++ designer.js
@@ -1,5 +1,22 @@
-﻿(function(AutoForm) {
+﻿(function(Form) {
+    "use strict";
 
-    AutoForm.containerChildrenAreSubWidgets();
-
+    Form.doAfter('init', function() {
+        var oldDs = this.datasource.boundDatasource();
+        this.subscribe('datasourceBindingChange', 'datasource', function(event) {
+            var ds = this.datasource.boundDatasource();
+            if(ds) {
+                if(!oldDs || oldDs.datasourceName !== ds.datasourceName) {
+                    this.autoForm();
+                }
+                oldDs = ds;
+                this.fields.show();
+            } else {
+                this.fields.hide();
+            }
+        }, this);
+            if(!this.datasource.boundDatasource()) {
+                this.fields.hide();
+            }
+    });
 });
--- widget.js
+++ widget.js
@@ -1,125 +1,59 @@
-﻿WAF.define('AutoForm', ['waf-core/widget', 'AutoFormNavigation', 'Text', 'Label', 'TextInput', 'DropDown', 'CheckBox'], function(widget, AutoFormNavigation, Text, Label, TextInput, DropDown, Checkbox) {
+﻿WAF.define('Form', ['waf-core/widget', 'FormNavigation', 'FormField', 'Text'], function(widget, FormNavigation, FormField, Text) {
     "use strict";
 
-    var AutoForm = widget.create('AutoForm', {
+    var Form = widget.create('Form', {
         datasource: widget.property({ type: 'datasource' }),
         fields: widget.property({
             type: 'list',
             attributes: [
                 { name: 'attribute', type: 'attribute', datasourceProperty: 'datasource' },
-                { name: 'label', type: 'string' },
+                { name: 'title', type: 'string' },
                 { name: 'widget', type: 'string', toolbox: true },
                 { name: 'property', type: 'string', toolbox: true }
             ]
         }),
         init: function() {
             this._previousDatasource = this.datasource();
-            this.subscribe('datasourceBindingChange', 'datasource', datasourceChange, this);
+            this.subscribe('datasourceBindingChange', 'datasource', function() {
+                this.invoke('datasource', this.datasource());
+            }, this);
             this.getPart('navigation').datasource(this.datasource());
 
             this.getPart('navigation').subscribe('search', function() {
                 var query = [];
                 var params = [];
-                var dataClass = this.datasource().getDataClass();
-                this.widgets().forEach(function(widget) {
-                    if(widget.isInstanceOf(Label)) {
-                        return;
-                    }
-                    widget.constructor.getProperties().forEach(function(name) {
-                        if(!widget[name].boundDatasource) {
-                            return;
-                        }
-                        var bound = widget[name].boundDatasource();
-                        if(!bound) {
-                            return;
-                        }
-                        if(bound.datasource !== this.datasource()) {
-                            return;
-                        }
-                        if(widget[name]()) {
-                            switch(getType(dataClass[bound.attribute])) {
-                                case 'string':
-                                    params.push(widget[name]());
-                                    query.push(bound.attribute + ' begin :' + params.length);
-                                    break;
-                                case 'relatedEntity':
-                                    var key;
-                                    if(dataClass instanceof WAF.DataClass) {
-                                        key = dataClass._private.primaryKey;
-                                    } else {
-                                        for(var k in dataClass) {
-                                            if(dataClass[k].isKey) {
-                                                key = k;
-                                            }
-                                        }
-                                    }
-                                    if(key) {
-                                        params.push(widget[name]());
-                                        query.push(bound.attribute + '.' + key + ' = :' + params.length);
-                                    }
-                                    break;
-                                default:
-                                    params.push(widget[name]());
-                                    query.push(bound.attribute + ' = :' + params.length);
-                            }
-                        }
-                    }.bind(this));
-                }.bind(this));
+                this.invoke('getQueryAndParams', query, params);
                 this.datasource().query(query.join(' AND '), { params: params });
             }, this);
+        },
+        autoForm: function() {
+            var dataClass = this.datasource.getDataClass();
+            var fields = [];
+            for(var attribute in dataClass) {
+                if(dataClass.hasOwnProperty(attribute)) {
+                    fields.push({ attribute: attribute });
+                }
+            }
+            this.fields(fields);
         }
     });
 
-    var TEXT_INPUT = { widget: TextInput, property: 'value' };
-    var DEFAULT_WIDGETS = {
-        'string': TEXT_INPUT,
-        'long':   TEXT_INPUT,
-        'number': TEXT_INPUT,
-        'float':  TEXT_INPUT,
-        'byte':   TEXT_INPUT,
-        'word':   TEXT_INPUT,
-        'long64': TEXT_INPUT,
-        //'date':
-        //'object':
-        'relatedEntity': { widget: DropDown, property: 'value', options: { 'items-attribute-label': 'ID', allowempty: true } },
-        'bool':         { widget: Checkbox, property: 'value' }
-    };
+    Form.inherit('waf-behavior/layout/composed');
+    Form.setPart('navigation', FormNavigation);
+    Form.setPart('title', Text);
 
-    var getType = function(attribute) {
-        if(attribute.kind !== 'storage') {
-            return attribute.kind;
+    Form.inherit('waf-behavior/layout/properties-container');
+    Form.linkListPropertyToContainer('fields', {
+        widgetClass: FormField,
+        getNewWidget: function(item, index) {
+            var bound = this.datasource.boundDatasource();
+            var widget = new FormField(WAF.extend({
+                datasource: bound && bound.toString(),
+            }, item));
+            return widget;
         }
-        return attribute.type;
-    };
+    });
 
-    var datasourceChange = function () {
-        this.getPart('navigation').datasource(this.datasource());
-        var bound = this.datasource.boundDatasource();
-        this.getPart('title').value(bound ? bound.datasourceName : 'No datasource');
-        //debugger;
-        this.detachAndDestroyAllWidgets();
-        var dataClass = this.datasource.getDataClass();
-        for(var name in dataClass) {
-            if(dataClass.hasOwnProperty(name)) {
-                var attribute = dataClass[name];
-                var defaultWidget = DEFAULT_WIDGETS[getType(attribute)];
-                if(defaultWidget) {
-                    this.attachWidget(new Label({ value: name }));
-                    var options = WAF.extend({}, defaultWidget.options || {});
-                    var widget = new defaultWidget.widget(options);
-                    this.attachWidget(widget);
-                    debugger;
-                    widget[defaultWidget.property].bindDatasource(bound.datasourceName + '.' + name);
-                }
-            }
-        }
-    };
-    AutoForm.inherit('waf-behavior/layout/composed');
-    AutoForm.inherit('waf-behavior/layout/container');
-
-    AutoForm.setPart('navigation', AutoFormNavigation);
-    AutoForm.setPart('title', Text);
-
-    return AutoForm;
+    return Form;
 
 });
